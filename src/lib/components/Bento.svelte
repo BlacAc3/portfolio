87b5f7a708680fe575194c966ac02c3a2c0e0cea
blacac3 <aceezeala@gmail.com>
2025-05-22 16:52:48 +0100

    completed animation

--- src/lib/components/Bento.svelte
+++ src/lib/components/Bento.svelte
@@ -1,8 +1,9 @@
 <script>
     import { onMount, onDestroy } from "svelte";
     import { writable } from "svelte/store";
-    import MagicCard from "./ui/MagicCard.svelte";
-    import BoxReveal from "./ui/BoxReveal.svelte";
+    import BoxReveal from "$lib/components/ui/BoxReveal.svelte";
+    import Me from "$lib/components/Me.svelte";
+    import { X } from "lucide-svelte";
 
     let viewportHeight = writable(window.innerHeight);
     let numberOfRows = 2; // You can update this if you have dynamic item count logic
@@ -11,10 +12,13 @@
     function updateViewportHeight() {
         viewportHeight.set(window.innerHeight - 100);
     }
+    const toggleExpand = () => {
+        expanded = !expanded;
+    };
 
     // Subscribe to viewportHeight and log its value
     const unsubscribe = viewportHeight.subscribe((value) => {
-        console.log("Viewport height changed:", value);
+        // console.log("Viewport height changed:", value);
     });
 
     onMount(() => {
@@ -26,6 +30,10 @@
         window.removeEventListener("resize", updateViewportHeight);
         unsubscribe(); // Unsubscribe from the store
     });
+
+    const cardOnClickHandler = () => {
+        expanded = true;
+    };
 </script>
 
 <div class="text-white flex justify-center items-center w-full">
@@ -37,40 +45,11 @@
             style="grid-auto-rows: {$viewportHeight / numberOfRows}px;"
         >
             {#if expanded}
-                <div class="bg-[#4A2E19] col-span-5"></div>
+                <Me expanded={false} />
+                {console.log(expanded)}
             {/if}
-            <div
-                class={`${!expanded ? "col-span-5" : ""} p-6 bg-[#4A2E19]/60 duration-100 ease-in-out hover:scale-[1.02] text-slate-300 flex flex-col gap-4 border border-orange-300/40 justify-end rounded-md cursor-pointer shadow-[#2C2521] hover:cursor-pointer shadow-md whitespace-nowrap text-4xl group transition-all duration-400`}
-                class:expanded
-                on:click={() => (expanded = !expanded)}
-            >
-                {#if expanded}
-                    <button
-                        class="absolute top-4 right-4 text-white z-50"
-                        on:click|stopPropagation={() => (expanded = false)}
-                    >
-                        ✕
-                    </button>
-                {/if}
-                <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                    <h1
-                        class="text-slate-300 text-5xl group-hover:text-brown-700 transition-all duration-500 flex justify-start font-semibold"
-                    >
-                        Ezeala Aaron
-                    </h1>
-                </BoxReveal>
-                <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                    <p class="text-2xl">Software Engineer</p>
-                </BoxReveal>
+            <Me on:expand={toggleExpand} {expanded} />
 
-                <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                    <p class="text-sm italic text-left text-wrap">
-                        “Turning coffee into code, bugs into features, and ideas
-                        into pixel-perfect reality.” - Ezeala Aaron
-                    </p>
-                </BoxReveal>
-            </div>
-            <!-- <div class="flex w-full flex-col gap-4 h-[250px] lg:flex-row"></div> -->
             <div
                 class="col-span-3 p-6 bg-[#4A2E19]/60 duration-100 ease-in-out hover:scale-[1.02] text-slate-300 flex flex-col gap-8 border border-orange-300/40 justify-end rounded-md cursor-pointer shadow-[#2C2521] hover:cursor-pointer shadow-md whitespace-nowrap text-4xl group transition-all"
             ></div>
@@ -89,9 +68,3 @@
         </ul>
     </div>
 </div>
-
-<style>
-    .expanded {
-        @apply fixed top-8 left-8 right-8 bottom-8 z-40 rounded-2xl shadow-xl;
-    }
-</style>
---
+++ src/lib/components/Me.svelte
@@ -0,0 +1,50 @@
+<script>
+    import BoxReveal from "./ui/BoxReveal.svelte";
+    import { X } from "lucide-svelte";
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    export let expanded = false;
+
+    function toggleExpand() {
+        dispatch("expand", !expanded); // simple boolean payload
+    }
+</script>
+
+<div
+    class={` ${!expanded ? "bg-[#4A2E19]/60 col-span-5 hover:scale-[1.02]" : "bg-[#4A2E19]/60 "} p-6 duration-100 ease-in-out  text-slate-300 flex flex-col gap-4 border border-orange-300/40 justify-end rounded-md cursor-pointer shadow-[#2C2521] hover:cursor-pointer shadow-md whitespace-nowrap text-4xl group transition-all `}
+    class:expanded
+    on:click={toggleExpand}
+>
+    {#if expanded}
+        <button
+            class="absolute top-4 right-4 text-white z-50"
+            on:click|stopPropagation={toggleExpand}
+        >
+            <X />
+        </button>
+    {/if}
+    <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <h1
+            class="text-slate-300 text-5xl group-hover:text-brown-700 transition-all duration-500 flex justify-start font-semibold"
+        >
+            Ezeala Aaron
+        </h1>
+    </BoxReveal>
+    <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <p class="text-2xl">Software Engineer</p>
+    </BoxReveal>
+
+    <BoxReveal boxColor={"#5046e6"} duration={0.5}>
+        <p class="text-sm italic text-left text-wrap">
+            “Turning coffee into code, bugs into features, and ideas into
+            pixel-perfect reality.” - Ezeala Aaron
+        </p>
+    </BoxReveal>
+</div>
+
+<style>
+    .expanded {
+        @apply fixed top-8 left-8 right-8 bottom-8 z-40 rounded-2xl shadow-xl;
+    }
+</style>
