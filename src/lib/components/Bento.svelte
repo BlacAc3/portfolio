7c9528e419461c30fe15932b4052298a56b497a3
blacac3 <aceezeala@gmail.com>
2025-05-22 09:43:33 +0100

    expansion feature

--- src/lib/components/Bento.svelte
+++ src/lib/components/Bento.svelte
@@ -6,6 +6,7 @@
 
     let viewportHeight = writable(window.innerHeight);
     let numberOfRows = 2; // You can update this if you have dynamic item count logic
+    let expanded = false;
 
     function updateViewportHeight() {
         viewportHeight.set(window.innerHeight - 100);
@@ -35,9 +36,22 @@
             class="grid grid-cols-12 gap-4 w-full"
             style="grid-auto-rows: {$viewportHeight / numberOfRows}px;"
         >
+            {#if expanded}
+                <div class="bg-[#4A2E19] col-span-5"></div>
+            {/if}
             <div
-                class="col-span-5 p-6 bg-[#4A2E19]/60 duration-100 ease-in-out hover:scale-[1.02] text-slate-300 flex flex-col gap-8 border border-orange-300/40 justify-end rounded-md cursor-pointer shadow-[#2C2521] hover:cursor-pointer shadow-md whitespace-nowrap text-4xl group transition-all duration-400"
+                class={`${!expanded ? "col-span-5" : ""} p-6 bg-[#4A2E19]/60 duration-100 ease-in-out hover:scale-[1.02] text-slate-300 flex flex-col gap-4 border border-orange-300/40 justify-end rounded-md cursor-pointer shadow-[#2C2521] hover:cursor-pointer shadow-md whitespace-nowrap text-4xl group transition-all duration-400`}
+                class:expanded
+                on:click={() => (expanded = !expanded)}
             >
+                {#if expanded}
+                    <button
+                        class="absolute top-4 right-4 text-white z-50"
+                        on:click|stopPropagation={() => (expanded = false)}
+                    >
+                        ✕
+                    </button>
+                {/if}
                 <BoxReveal boxColor={"#5046e6"} duration={0.5}>
                     <h1
                         class="text-slate-300 text-5xl group-hover:text-brown-700 transition-all duration-500 flex justify-start font-semibold"
@@ -50,7 +64,7 @@
                 </BoxReveal>
 
                 <BoxReveal boxColor={"#5046e6"} duration={0.5}>
-                    <p class="text-sm italic text-wrap">
+                    <p class="text-sm italic text-left text-wrap">
                         “Turning coffee into code, bugs into features, and ideas
                         into pixel-perfect reality.” - Ezeala Aaron
                     </p>
@@ -75,3 +89,9 @@
         </ul>
     </div>
 </div>
+
+<style>
+    .expanded {
+        @apply fixed top-8 left-8 right-8 bottom-8 z-40 rounded-2xl shadow-xl;
+    }
+</style>
